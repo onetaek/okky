f93a1d937695d85156591a5bd45decba0f9421b7
KimJuneHyuk <myhyuk9172@naver.com>
2023-01-13 13:25:38 +0900

    23.01.09  2 차 수정. 올림

--- src/main/webapp/resources/init.sql
+++ src/main/webapp/resources/init.sql
@@ -24,15 +24,22 @@ CREATE TABLE `okky`.`contactCountries`
 
 CREATE TABLE `okky`.`telecoms`
 (
-    `value` VARCHAR(10) NOT NULL, #SKT, KT, LGU, 알뜰폰
-            `text`  VARCHAR(20) NOT NULL,
+    `value` VARCHAR(10) NOT NULL,
+    #SKT,
+    KT,
+    LGU,
+    알뜰폰
+            `text` VARCHAR(20) NOT NULL,
     CONSTRAINT PRIMARY KEY (`value`)
 )engine = InnoDB default charset=utf8;
 
 CREATE TABLE `okky`.`statuses`
 (
-    `value` VARCHAR(3)  NOT NULL, # OKY , SUS, RES
-            `text`  VARCHAR(20) NOT NULL,
+    `value` VARCHAR(3) NOT NULL,
+    #       OKY,
+    SUS,
+    RES
+            `text` VARCHAR(20) NOT NULL,
     CONSTRAINT PRIMARY KEY (`value`)
 )engine = InnoDB default charset=utf8;
 
@@ -51,7 +58,7 @@ CREATE TABLE `okky`.`contactAuth`
 CREATE TABLE `okky`.`emailAuth`
 (
     `index`     INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `email`     VARCHAR(100)  NOT NULL,
+    `email`     VARCHAR(100) NOT NULL,
     `code`      VARCHAR(6)   NOT NULL,
     `createdAt` DATETIME     NOT NULL DEFAULT NOW(),
     `expiresAt` DATETIME     NOT NULL,
@@ -62,17 +69,23 @@ CREATE TABLE `okky`.`emailAuth`
 
 CREATE TABLE `okky`.`users`
 (
-    `email`             VARCHAR(100) NOT NULL,
-    `password`          VARCHAR(128) NOT NULL,
-    `name`              VARCHAR(20)  NOT NULL,
-    `nickName`          VARCHAR(20)  NOT NULL UNIQUE,
-    `telecom`           VARCHAR(10)  NOT NULL, # 0FK
-        `contactCountryValue` VARCHAR(3)   NOT NULL, # 082, 0FK
-                        `contact`           VARCHAR(20)  NOT NULL,
-    `policyEmailSend`   boolean      NOT NULL DEFAULT FALSE,
-    `createdAt`         DATETIME     NOT NULL DEFAULT NOW(),
-    `statusValue`       VARCHAR(3)   NOT NULL, #정상 회원, 일시 정지 회원, 탈퇴 회원 등....  0FK
-        `isAdmin`           boolean      NOT NULL DEFAULT FALSE,
+    `email`           VARCHAR(100) NOT NULL,
+    `password`        VARCHAR(128) NOT NULL,
+    `name`            VARCHAR(20)  NOT NULL,
+    `nickName`        VARCHAR(20)  NOT NULL UNIQUE,
+    `telecom`         VARCHAR(10)  NOT NULL,
+    #                 0FK
+        `contactCountryValue` VARCHAR (3) NOT NULL,
+    # 082,
+    0FK
+                      `contact` VARCHAR(20) NOT NULL,
+    `policyEmailSend` boolean      NOT NULL DEFAULT FALSE,
+    `createdAt`       DATETIME     NOT NULL DEFAULT NOW(),
+    `statusValue`     VARCHAR(3)   NOT NULL DEFAULT 'OKY',
+    #정상               회원,
+    일시                정지 회원,
+    탈퇴                회원 등....0FK
+        `isAdmin` boolean NOT NULL DEFAULT FALSE,
 
     CONSTRAINT PRIMARY KEY (`email`),
     CONSTRAINT UNIQUE (`telecom`, `contact`, `contactCountryValue`),
@@ -89,14 +102,13 @@ CREATE TABLE `okky`.`users`
 
 CREATE TABLE `okky`.`articles`
 (
-    `index`     INT UNSIGNED    NOT NULL AUTO_INCREMENT,
-    `boardId`   VARCHAR(10)     NOT NULL, #
-                `tag`       VARCHAR(20)     NOT NULL,
-    `userEmail` VARCHAR(100)    NOT NULL,
-    `title`     VARCHAR(100)    NOT NULL,
-    `content`   VARCHAR(5000)   NOT NULL,
+    `index`     INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `boardId`   VARCHAR(10)   NOT NULL,
+    `userEmail` VARCHAR(100)  NOT NULL,
+    `title`     VARCHAR(100)  NOT NULL,
+    `content`   VARCHAR(5000) NOT NULL,
     `view`      BIGINT UNSIGNED NOT NULL DEFAULT 0,
-    `createdAt` DATETIME        NOT NULL DEFAULT NOW(),
+    `createdAt` DATETIME      NOT NULL DEFAULT NOW(),
     CONSTRAINT PRIMARY KEY (`index`),
     CONSTRAINT FOREIGN KEY (`boardId`) REFERENCES `okky`.`boards` (`id`)
         ON UPDATE CASCADE
@@ -104,9 +116,6 @@ CREATE TABLE `okky`.`articles`
     CONSTRAINT FOREIGN KEY (`userEmail`) REFERENCES `okky`.`users` (`email`)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT FOREIGN KEY (`tag`) REFERENCES `okky`.`tags` (`value`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
 )engine = InnoDB default charset=utf8;
 
 CREATE TABLE `okky`.`articleLikes`
@@ -126,7 +135,7 @@ CREATE TABLE `okky`.`articleLikes`
 CREATE TABLE `okky`.`comments`
 (
     `index`        INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `commentIndex` INT UNSIGNED NULL     DEFAULT NULL,
+    `commentIndex` INT UNSIGNED NULL DEFAULT NULL,
     `articleIndex` INT UNSIGNED NOT NULL,
     `userEmail`    VARCHAR(100) NOT NULL,
     `content`      VARCHAR(200) NOT NULL,
@@ -143,17 +152,38 @@ CREATE TABLE `okky`.`comments`
         ON DELETE CASCADE
 )engine = InnoDB default charset=utf8;
 
+create table `okky`.`tagOfArticle`
+(
+    `index` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `articleIdx` INT UNSIGNED    NOT NULL,
+    `tagValue` VARCHAR(20) NOT NULL,
+    `writtenAt` datetime not null default now(),
+    constraint primary key (`index`),
+    constraint foreign key (`articleIdx`) references `okky`.articles(`index`)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    constraint foreign key (`tagValue`) references `okky`.tags(`value`)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) engine = InnoDB
+  default charset = utf8;
 
 
 
+insert into `okky`.`telecoms` (value, text)
+values ('KT', 'KT');
+insert into `okky`.`telecoms` (value, text)
+values ('LGU+', 'LGU+');
+insert into `okky`.`telecoms` (value, text)
+values ('SKT', 'SKT');
 
-insert into `okky`.`telecoms` (value, text) values ('KT','KT');
-insert into `okky`.`telecoms` (value, text) values ('LGU+','LGU+');
-insert into `okky`.`telecoms` (value, text) values ('SKT','SKT');
+insert into `okky`.`contactCountries` (value, text)
+values ('082', '대한민국');
 
-insert into `okky`.`contactCountries` (value, text) values ('082','대한민국');
-
-insert into `okky`.`boards` (id,text) values('1','Q&A');
-insert into `okky`.`boards` (id,text) values ('2','커뮤니티');
-insert into `okky`.`boards` (id,text) values('3','공지사항');
+insert into `okky`.`boards` (id, text)
+values ('1', 'Q&A');
+insert into `okky`.`boards` (id, text)
+values ('2', '커뮤니티');
+insert into `okky`.`boards` (id, text)
+values ('3', '공지사항');
 
