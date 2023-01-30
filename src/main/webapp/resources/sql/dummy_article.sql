3011398ef4019963f32a466b7a0719b96499e6e6
KimJuneHyuk <myhyuk9172@naver.com>
2023-01-30 09:22:24 +0900

    23.01.25  21 차 수정. 올림

--- src/main/webapp/resources/sql/dummy_article.sql
+++ src/main/webapp/resources/sql/dummy_article.sql
@@ -7,7 +7,7 @@ BEGIN
     DECLARE i INT DEFAULT 1;
     WHILE i <= 200 DO
             INSERT INTO `okky`.articles( boardId, userEmail, title, content)
-            VALUES (2,concat('dnjsxorchlrh@naver.com'),concat('더미 제목',i),concat('더미 내용',i));
+            VALUES (2,concat('kjh65659172@gmail.com'),concat('더미 제목',i),concat('더미 내용',i));
             SET i = i + 1;
 END WHILE;
 END$$
